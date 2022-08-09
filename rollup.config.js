97d972c4da47d5089fab07a2fe371ba51d44c14f
xieyuhang <13451213869@163.com>
2022-08-09 22:19:02 +0800

    add cjs output

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,17 @@ import typescript from 'rollup-plugin-typescript2'
 
 export default defineConfig({
   input: path.resolve(__dirname, 'src/index.ts'),
-  output: {
-    file: 'dist/index.mjs',
-    format: 'esm'
-  },
+  output: [
+    {
+      file: 'dist/index.mjs',
+      format: 'esm'
+    },
+    {
+      file: 'dist/index.cjs',
+      format: 'cjs',
+      exports: 'default'
+    }
+  ],
   plugins: [
     typescript()
   ]
