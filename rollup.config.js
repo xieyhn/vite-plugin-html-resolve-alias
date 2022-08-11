c78e38ddc183185ede09035d52d6994e215a06d2
xieyuhang <13451213869@163.com>
2022-08-11 10:06:54 +0800

    add rollup dts plguin

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,34 @@
 import path from 'path'
 import { defineConfig } from 'rollup'
 import typescript from 'rollup-plugin-typescript2'
+import dts from 'rollup-plugin-dts'
 
-export default defineConfig({
-  input: path.resolve(__dirname, 'src/index.ts'),
-  output: [
-    {
-      file: 'dist/index.mjs',
+export default defineConfig([
+  {
+    input: path.resolve(__dirname, 'src/index.ts'),
+    output: [
+      {
+        file: 'dist/index.mjs',
+        format: 'esm'
+      },
+      {
+        file: 'dist/index.cjs',
+        format: 'cjs',
+        exports: 'default'
+      }
+    ],
+    plugins: [
+      typescript()
+    ]
+  },
+  {
+    input: path.resolve(__dirname, 'src/index.ts'),
+    output: {
+      file: 'dist/index.d.ts',
       format: 'esm'
     },
-    {
-      file: 'dist/index.cjs',
-      format: 'cjs',
-      exports: 'default'
-    }
-  ],
-  plugins: [
-    typescript()
-  ]
-})
+    plugins: [
+      dts()
+    ]
+  }
+])
